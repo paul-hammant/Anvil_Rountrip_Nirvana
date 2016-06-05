725ab4011331e2bd8a89bdda6ce015a688c632a7
Anvil <contact@anvil.works>
2016-06-05 15:39:49 +0000

    Saved from web IDE

---
+++ forms/Form1.py
@@ -0,0 +1,17 @@
+from anvil import *
+
+class Form1(Form1Template):
+
+  def __init__(self):
+    # This sets up a variable for every component on this form.
+    # For example, if we've drawn a button called "send_button", we can
+    # refer to it as self.send_button:
+    self.init_components()
+
+  def btn_say_hello_click(self, sender, **event_args):
+    # This function gets called when the button "hello_btn" gets clicked
+    self.lbl_hello.text = "Hello, " + self.txt_name.text + "!"
+    
+    
+    
+    
